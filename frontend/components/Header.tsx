5bbaa6bdd3c1c9bfe01c97aca84f81f6cd75d22a
Harry <stuartharry0313@gmail.com>
2022-07-21 15:51:16 +0100

    Fix size of boldness title

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Header: NextPage = () => {
   return (
     <header className='px-5 py-1 bg-blue-500'>
-        <h1 className='text-2xl font-extrabold'>Atorus</h1>
+        <h1 className='text-2xl font-bold'>Atorus</h1>
     </header>
   )
 }
