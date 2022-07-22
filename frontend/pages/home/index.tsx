ffdb944a186383b2f1d96861691052376339ac46
Harry <stuartharry0313@gmail.com>
2022-07-22 23:32:12 +0100

    Wrap modal with background

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -24,26 +24,26 @@ function Home() {
             </ul>
         </div>
         <Modal open={showModal} onClose={() => setShowModal(false)}>
-          {
-            currentPost ?
-              <div style={{ transform: 'translate(-50%, -50%)', zIndex: 1000 }} className="fixed top-1/2 left-1/2 bg-[#000000] h-[90%] w-[80%] overflow-y-scroll xl:overflow-y-hidden">
-                <div className="flex flex-col xl:flex-row h-full">
-                  <div className="mx-auto h-full">
-                    <img src={currentPost.image} alt="Heya" className="max-h-full mx-auto" />
-                  </div>
-                  <div className="text-white w-[30%] border-l">
-                    <div>
-                      <a href='https://google.com' className="flex flex-row pl-2 border-b p-5 pb-3">
-                          <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
-                          <span className='align-middle inline-block ml-1 mt-0.5'><strong>{currentPost.owner}</strong> to album &quot;Something&quot;</span>
-                      </a>
-                      <span className="p-5 block"><strong>{currentPost.owner}</strong> {currentPost.caption}</span>
+          <div style={{ transform: 'translate(-50%, -50%)', zIndex: 1000 }} className="fixed top-1/2 left-1/2 bg-[#000000] h-[90%] w-[80%] overflow-y-scroll xl:overflow-y-hidden text-white">
+            {
+              currentPost ?
+                  <div className="flex flex-col xl:flex-row h-full">
+                    <div className="mx-auto h-full">
+                      <img src={currentPost.image} alt="Heya" className="object-cover max-h-full align-middle" />
+                    </div>
+                    <div className="text-white w-[30%] border-l">
+                      <div>
+                        <a href='https://google.com' className="flex flex-row pl-2 border-b p-5 pb-3">
+                            <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
+                            <span className='align-middle inline-block ml-1 mt-0.5'><strong>{currentPost.owner}</strong> to album &quot;Something&quot;</span>
+                        </a>
+                        <span className="p-5 block"><strong>{currentPost.owner}</strong> {currentPost.caption}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            : <h1>Oops looks like something went wrong</h1>
-          }
+              : <h1>Oops looks like something went wrong</h1>
+            }
+          </div>
         </Modal>
     </div>
   );
