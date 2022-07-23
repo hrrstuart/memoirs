c0cc9207f6bfd4b15289caa4b84a991cae8f79a0
Harry <stuartharry0313@gmail.com>
2022-07-23 22:43:59 +0100

    Fix href to redirect to owner of photo profile

--- frontend/components/Posts/Post.tsx
+++ frontend/components/Posts/Post.tsx
@@ -8,7 +8,7 @@ export default function Post(props: { post: IPost, setModal: (post: IPost) => vo
         <img className='object-cover rounded-lg' src={props.post.image} alt={`Photo uploaded by ${props.post.owner}`} />
         <div className="text-gray-300 absolute h-full w-full bottom-0 rounded-lg bg-gradient-to-t from-black/30 group-hover:bg-black/10 duration-75">
             <div className='w-full h-full relative'>
-                <a href='https://google.com' className="absolute bottom-0 left-0 flex flex-row pb-2 pl-2">
+                <a href={`/user/${props.post.owner}`} className="absolute bottom-0 left-0 flex flex-row pb-2 pl-2">
                     <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
                     <span className='align-middle inline-block ml-1 mt-0.5'><strong>{props.post.owner}</strong> to album &quot;Something&quot;</span>
                 </a>
