12cd6c035a495c26ecdc03bb438b19228f1689f0
Harry <stuartharry0313@gmail.com>
2022-07-24 23:05:34 +0100

    Add div overlay while hovering on items in grid

--- frontend/components/profile/Photos.tsx
+++ frontend/components/profile/Photos.tsx
@@ -23,12 +23,16 @@ function Photos() {
     const post = typeof(item) === 'string';
 
     return (
-      <div className='relative'>
+      <div className='relative group cursor-pointer'>
         <img className='object-cover w-52 h-52' src={post ? item : item.thumbnail!} alt="Something" />
-        { !post && 
+        <div className='absolute w-full h-full group-hover:bg-black/20 top-0 duration-75'></div>
+        { !post ?
           <div className='absolute bottom-0 bg-black/70 w-full text-center py-1'>
             <div className='font-bold text-lg'>{item.title}</div>
             <div className='text-gray-400 text-sm'><strong>{item.photos}</strong> photos</div>
+          </div> : 
+          <div className='absolute top-0 left-0 right-0 bottom-0 m-auto hidden group-hover:block'>
+            <div>Shtory bud?</div>
           </div>
         }
       </div>
