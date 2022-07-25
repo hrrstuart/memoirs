03a59bd38fc6f2ea088870bcef126628bc3cdebe
Harry <stuartharry0313@gmail.com>
2022-07-25 22:27:43 +0100

    Allow for optional styling to component

--- frontend/components/base-components/ImageOverlay.tsx
+++ frontend/components/base-components/ImageOverlay.tsx
@@ -5,13 +5,24 @@ function ImageOverlay(props: {
     alt: string;
     height: string;
     width: string;
+    parentStyling?: {
+        rounded?: string;
+        gradient?: string;
+    };
+    imageStyling?: {
+        rounded?: string;
+    }
     children: JSX.Element
 }) {
+  const parent = props.parentStyling?.gradient + ' ' + props.parentStyling?.rounded;
+  const image = props.imageStyling?.rounded + ' ' + props.height + ' ' + props.width;
+
   return (
-    <div className='relative group'>
-        <img className={`object-cover h-${props.height} w-${props.width}`} src={props.image} alt={props.alt} />
-        <div className='absolute w-full h-full group-hover:bg-black/20 top-0 duration-75' />
-        { props.children }
+    <div className='relative group cursor-pointer'>
+        <img className={`object-cover ${image}`} src={props.image} alt={props.alt} />
+        <div className={`absolute w-full h-full group-hover:bg-black/20 top-0 duration-75 ${parent}`}>
+            { props.children }
+        </div>
     </div>
   )
 }
