e22f0bfe235cb251106a7825ff6e2e6deb731b06
Harry <stuartharry0313@gmail.com>
2022-07-25 21:36:33 +0100

    Add album interface

---
+++ frontend/util/types/albums/album.ts
@@ -0,0 +1,15 @@
+import { IPost } from "../posts/post";
+
+export interface IAlbum {
+    ownerID: string;
+    information: {
+        title: string;
+        description: string;
+        createdAt: string;
+        updatedAt: string;
+    }
+    posts: IPost[];
+    settings: {
+        admins: string[];
+    }
+}
