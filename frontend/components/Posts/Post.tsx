55f533eb69831494666a0b6b214fbcf1fcc3f123
Harry <stuartharry0313@gmail.com>
2022-07-25 22:26:37 +0100

    Implement ImageOverlay component

--- frontend/components/Posts/Post.tsx
+++ frontend/components/Posts/Post.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import { IPost } from '../../util/types/posts/post'
+import ImageOverlay from '../base-components/ImageOverlay'
 
 export default function Post(props: { post: IPost, setModal: (post: IPost) => void }) {
   return (
-    <div onClick={() => props.setModal(props.post)} className="relative group hover:cursor-pointer">
-        <img className='object-cover rounded-lg' src={props.post.image} alt={`Photo uploaded by ${props.post.owner}`} />
-        <div className="text-gray-300 absolute h-full w-full bottom-0 rounded-lg bg-gradient-to-t from-black/30 group-hover:bg-black/10 duration-75">
-            <div className='w-full h-full relative'>
-                <a href={`/user/${props.post.owner}`} className="absolute bottom-0 left-0 flex flex-row pb-2 pl-2">
-                    <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
-                    <span className='align-middle inline-block ml-1 mt-0.5'><strong>{props.post.owner}</strong> to album &quot;Something&quot;</span>
-                </a>
-            </div>
-        </div>
+    <div onClick={() => props.setModal(props.post)}>
+        <ImageOverlay 
+          parentStyling={{ gradient: "bg-gradient-to-t from-black/30", rounded: "rounded-lg" }}
+          imageStyling={{ rounded: "rounded-lg" }}
+          image={props.post.image}
+          alt={`Photo uploaded by ${props.post.owner}`}
+          height='w-full' width='w-full'
+        >
+          <a href={`/user/${props.post.owner}`} className="absolute bottom-0 left-0 flex flex-row pb-2 pl-2 text-white font-medium">
+              <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
+              <span className='align-middle inline-block ml-1 mt-0.5'><strong className='text-gray-300'>{props.post.owner}</strong> to album &quot;Something&quot;</span>
+          </a>
+        </ImageOverlay>
     </div>
   )
 }
