1abf39069638cfc585d36ee87fba41fbb8c2abaa
Harry <stuartharry0313@gmail.com>
2022-07-26 21:43:28 +0100

    Add new post modal format for posts

---
+++ frontend/components/base-components/PostModal.tsx
@@ -0,0 +1,36 @@
+import React, { SetStateAction } from 'react'
+import { IPost } from '../../util/types/posts/post';
+import Modal from '../Modal';
+
+function PostModal(props: {
+    open: boolean,
+    post: IPost | null | undefined,
+    setShowModal: (value: SetStateAction<boolean>) => void
+}) {
+  return (
+    <Modal open={props.open} onClose={() => { document.body.classList.remove("overflow-hidden"); props.setShowModal(false) }}>
+      <div style={{ transform: 'translate(-50%, -50%)', zIndex: 1000 }} className="fixed top-1/2 left-1/2 bg-[#000000] h-[90%] w-[80%] overflow-y-scroll xl:overflow-y-hidden text-white">
+        {
+          props.post ?
+              <div className="flex flex-col xl:flex-row h-full">
+                <div className="mx-auto h-full flex items-center justify-center">
+                  <img src={props.post.image} alt="Heya" className="object-cover max-h-full" />
+                </div>
+                <div className="text-white w-[30%] border-l">
+                  <div>
+                    <a href='https://google.com' className="flex flex-row pl-2 border-b p-5 pb-3">
+                        <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
+                        <span className='align-middle inline-block ml-1 mt-0.5'><strong>{props.post.owner}</strong> to album &quot;Something&quot;</span>
+                    </a>
+                    <span className="p-5 block"><strong>{props.post.owner}</strong> {props.post.caption}</span>
+                  </div>
+                </div>
+              </div>
+          : <h1>Oops looks like something went wrong</h1>
+        }
+      </div>
+    </Modal>
+  )
+}
+
+export default PostModal
--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PostModal from "../../components/base-components/PostModal";
 import Modal from "../../components/Modal";
 import Post from "../../components/photo-stream/Post";
 import { IPost } from "../../util/types/posts/post";
@@ -25,28 +26,7 @@ function Home() {
                 <Post setModal={setModal} post={{ caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/group-of-young-people-having-water-fight-in-field-laughing-picture-id200250724-002", likes: 13812, owner: "hrrstuart" }} />
             </ul>
         </div>
-        <Modal open={showModal} onClose={() => { document.body.classList.remove("overflow-hidden"); setShowModal(false) }}>
-          <div style={{ transform: 'translate(-50%, -50%)', zIndex: 1000 }} className="fixed top-1/2 left-1/2 bg-[#000000] h-[90%] w-[80%] overflow-y-scroll xl:overflow-y-hidden text-white">
-            {
-              currentPost ?
-                  <div className="flex flex-col xl:flex-row h-full">
-                    <div className="mx-auto h-full flex items-center justify-center">
-                      <img src={currentPost.image} alt="Heya" className="object-cover max-h-full" />
-                    </div>
-                    <div className="text-white w-[30%] border-l">
-                      <div>
-                        <a href='https://google.com' className="flex flex-row pl-2 border-b p-5 pb-3">
-                            <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
-                            <span className='align-middle inline-block ml-1 mt-0.5'><strong>{currentPost.owner}</strong> to album &quot;Something&quot;</span>
-                        </a>
-                        <span className="p-5 block"><strong>{currentPost.owner}</strong> {currentPost.caption}</span>
-                      </div>
-                    </div>
-                  </div>
-              : <h1>Oops looks like something went wrong</h1>
-            }
-          </div>
-        </Modal>
+        <PostModal open={showModal} post={currentPost} setShowModal={setShowModal}  />
     </div>
   );
 }
