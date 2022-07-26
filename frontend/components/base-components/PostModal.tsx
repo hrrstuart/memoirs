e293403e0aaca6d6fa5a5fb5169977da7ed32384
Harry <stuartharry0313@gmail.com>
2022-07-26 22:29:30 +0100

    Redirect to account

--- frontend/components/base-components/PostModal.tsx
+++ frontend/components/base-components/PostModal.tsx
@@ -18,7 +18,7 @@ function PostModal(props: {
                 </div>
                 <div className="text-white w-[30%] border-l">
                   <div>
-                    <a href='https://google.com' className="flex flex-row pl-2 border-b p-5 pb-3">
+                    <a href={`/user/${props.post.owner}`} className="flex flex-row pl-2 border-b p-5 pb-3">
                         <img src="https://media.gettyimages.com/photos/picture-taken-07-october-2004-shows-google-founders-sergey-brin-and-picture-id76737519?s=2048x2048" className='rounded-full object-cover h-8 w-8 align-middle mr-2' alt="Something" />
                         <span className='align-middle inline-block ml-1 mt-0.5'><strong>{props.post.owner}</strong> to album &quot;Something&quot;</span>
                     </a>
