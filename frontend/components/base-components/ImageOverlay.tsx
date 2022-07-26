b11db9ee45e88dcf5ff91aed78975a761708b274
Harry <stuartharry0313@gmail.com>
2022-07-26 21:46:11 +0100

    Add property to allow for click events

--- frontend/components/base-components/ImageOverlay.tsx
+++ frontend/components/base-components/ImageOverlay.tsx
@@ -5,6 +5,7 @@ function ImageOverlay(props: {
     alt: string;
     height: string;
     width: string;
+    onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => any;
     parentStyling?: {
         rounded?: string;
         gradient?: string;
@@ -17,8 +18,12 @@ function ImageOverlay(props: {
   const parent = props.parentStyling?.gradient + ' ' + props.parentStyling?.rounded;
   const image = props.imageStyling?.rounded + ' ' + props.height + ' ' + props.width;
 
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    props.onClick && props.onClick(e);
+  }
+
   return (
-    <div className='relative group cursor-pointer'>
+    <div onClick={handleClick} className='relative group cursor-pointer'>
         <img className={`object-cover ${image}`} src={props.image} alt={props.alt} />
         <div className={`absolute w-full h-full group-hover:bg-black/20 top-0 duration-75 ${parent}`}>
             { props.children }
