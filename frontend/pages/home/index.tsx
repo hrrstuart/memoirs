93157fe4981d9d4fe5777072749de43c12a3cfc3
Harry <stuartharry0313@gmail.com>
2022-07-28 22:13:08 +0100

    Fix missing property error

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import PostModal from "../../components/base-components/PostModal";
-import Modal from "../../components/Modal";
 import Post from "../../components/photo-stream/Post";
 import { IPost } from "../../util/types/posts/post";
 
@@ -19,11 +18,11 @@ function Home() {
     <div className="w-full max-w-full md:max-w-md">
         <div className="flex">
             <ul className="flex flex-col space-y-6">
-                <Post setModal={setModal} post={{ caption: "Life must be the craziest things and when you get to enjoy it, it can be pretty mad. Here is a picture of my satellite in the 3rd person POV. Enjoy!", image: "https://64.media.tumblr.com/d28ed0d493a7794b9a7b490e3d6612ea/tumblr_pdrm0xx05J1wvnafdo1_1280.jpg", likes: 841, owner: "hrrstuart" }} />
-                <Post setModal={setModal} post={{ caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/spy-satellite-orbiting-earth-nasa-public-domain-imagery-picture-id1201649930?s=2048x2048", likes: 1238, owner: "hrrstuart" }} />
-                <Post setModal={setModal} post={{ caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/uschi-glas-dieter-hermann-and-heidi-kranz-attend-the-bild-place-to-b-picture-id462960080?s=2048x2048", likes: 2922, owner: "hrrstuart" }} />
-                <Post setModal={setModal} post={{ caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/van-halen-having-a-crazy-party-eating-hamburgers-and-potatoes-tokyo-picture-id593327617?s=2048x2048", likes: 2292, owner: "hrrstuart" }} />
-                <Post setModal={setModal} post={{ caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/group-of-young-people-having-water-fight-in-field-laughing-picture-id200250724-002", likes: 13812, owner: "hrrstuart" }} />
+                <Post setModal={setModal} post={{ createdAt: 1601948191013, caption: "Life must be the craziest things and when you get to enjoy it, it can be pretty mad. Here is a picture of my satellite in the 3rd person POV. Enjoy!", image: "https://64.media.tumblr.com/d28ed0d493a7794b9a7b490e3d6612ea/tumblr_pdrm0xx05J1wvnafdo1_1280.jpg", likes: 841, owner: "hrrstuart" }} />
+                <Post setModal={setModal} post={{ createdAt: 1601948194113, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/spy-satellite-orbiting-earth-nasa-public-domain-imagery-picture-id1201649930?s=2048x2048", likes: 1238, owner: "hrrstuart" }} />
+                <Post setModal={setModal} post={{ createdAt: 1601948114513, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/uschi-glas-dieter-hermann-and-heidi-kranz-attend-the-bild-place-to-b-picture-id462960080?s=2048x2048", likes: 2922, owner: "hrrstuart" }} />
+                <Post setModal={setModal} post={{ createdAt: 1601948192913, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/van-halen-having-a-crazy-party-eating-hamburgers-and-potatoes-tokyo-picture-id593327617?s=2048x2048", likes: 2292, owner: "hrrstuart" }} />
+                <Post setModal={setModal} post={{ createdAt: 1601948194213, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/group-of-young-people-having-water-fight-in-field-laughing-picture-id200250724-002", likes: 13812, owner: "hrrstuart" }} />
             </ul>
         </div>
         <PostModal open={showModal} post={currentPost} setShowModal={setShowModal}  />
