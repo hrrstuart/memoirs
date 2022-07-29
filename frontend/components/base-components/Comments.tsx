4def93c957a95439a44c64661c41d2fc610a72a6
Harry <stuartharry0313@gmail.com>
2022-07-29 23:15:11 +0100

    Add timeago.js to project

--- frontend/components/base-components/Comments.tsx
+++ frontend/components/base-components/Comments.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { IComment } from '../../util/types/posts/post';
-import { format, register } from 'timeago.js';
-import a from 'timeago.js/esm/lang/en_short';
-register("en_short", a);
+import { format } from 'timeago.js';
 
 
 function Comments() {
+
     const comments: IComment[] = [
-        { content: 'That is actually laughable mate', createdAt: 1659041056485, likes: 291, owner: 'liamk123', replyCount: 25 },
+        { content: 'That is actually laughable mate', createdAt: 1401958195819, likes: 291, owner: 'liamk123', replyCount: 25 },
         { content: 'That is actually laughable', createdAt: 1659041056485, likes: 291, owner: 'liamk123', replyCount: 25 },
         { content: 'That is actually laughable', createdAt: 1659041056485, likes: 291, owner: 'liamk123', replyCount: 25 },
         { content: 'That is actually laughable', createdAt: 1659041056485, likes: 291, owner: 'liamk123', replyCount: 25 },
@@ -21,8 +20,8 @@ function Comments() {
                 <span className='pr-2 break-words'><strong>{comment.owner}</strong> {comment.content}</span>
                 <button className='ml-auto mb-auto'>Like</button>
             </div>
-            <div className='flex flex-row space-x-10 mx-auto text-gray-200 text-sm'>
-                <div>{format(comment.createdAt, "en_short").replace(/ago/gi, "")}</div>
+            <div className='flex flex-row space-x-10 mx-auto text-gray-400 text-xs'>
+                <div>{format(comment.createdAt)}</div>
                 <div>View replies ({comment.replyCount.toLocaleString()})</div>
             </div>
         </div>
