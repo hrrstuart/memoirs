5226de27e296fb60de86cadf067dfe7815c45278
Harry <stuartharry0313@gmail.com>
2022-08-01 22:22:21 +0100

    Allow to pass album as profile tag

--- frontend/components/base-components/ProfileTag.tsx
+++ frontend/components/base-components/ProfileTag.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
-function ProfileTag({ image, name, optionalText, photoSize="8" }: {
+function ProfileTag({ image, name, albumID, optionalText, photoSize="8" }: {
     image: string;
     name: string;
+    albumID?: string;
     optionalText?: string;
     photoSize?: string;
 }) {
+  const link = albumID ? `/album/${albumID}` : `/user/${name}`;
+
   return (
-    <a href={`/user/${name}`} className="flex flex-row text-white font-medium">
-        <img src={image} className={`rounded-full object-cover h-${photoSize} w-${photoSize} align-middle mr-2`} alt="Something" />
-        <span className='align-middle inline-block ml-1 mt-0.5'>{name} {optionalText}</span>
+    <a href={link} className="flex flex-row space-x-2 text-white font-medium">
+        <img src={image} className={`rounded-full object-cover h-${photoSize} w-${photoSize} my-auto`} alt="Something" />
+        <span className='align-middle inline-block ml-1 mt-0.5'>
+          { 
+            !albumID ? 
+            <>{name} {optionalText}</> :
+            <div className='font-normal'>
+              <strong>&quot;{name}&quot;</strong> album
+            </div>
+          }
+        </span>
     </a>
   )
 }
