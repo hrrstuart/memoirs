167408c47ae35bf65b785ae402113311b295f80d
Harry <stuartharry0313@gmail.com>
2022-08-07 23:59:20 +0100

    Make follow type an enum

--- backend/src/entity/following/FollowType.ts
+++ backend/src/entity/following/FollowType.ts
@@ -1,12 +1,20 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum FollowingType {
+    ALBUM = "album",
+    USER = "user"
+}
+
 @Entity()
 export class FollowType {
 
     @PrimaryGeneratedColumn("increment")
     id: number;
 
-    @Column()
-    type: string;
+    @Column({
+        type: "enum",
+        enum: FollowingType
+    })
+    type: FollowingType
 
 }
--- backend/src/entity/reactions/ReactionType.ts
+++ backend/src/entity/reactions/ReactionType.ts
@@ -1,12 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum ReactionToType {
+    ALBUM = "album",
+    COMMENT = "comment",
+    POST = "post"
+}
+
 @Entity()
 export class ReactionType {
 
     @PrimaryGeneratedColumn("increment")
     id: number;
 
-    @Column()
-    type: string;
+    @Column({
+        type: "enum",
+        enum: ReactionToType
+    })
+    type: ReactionToType;
 
 }
