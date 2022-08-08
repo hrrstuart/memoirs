64af9c91ae01a420b18b8fab0c3f9176b22cead3
Harry <stuartharry0313@gmail.com>
2022-08-08 23:56:00 +0100

    Add typeorm decorators

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,16 +1,22 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class User {
+  @PrimaryGeneratedColumn("uuid")
   @Field((type) => String)
   id: string;
 
+  @Column()
   @Field()
   username: string;
 
+  @CreateDateColumn()
   @Field((type) => Int)
   created_at: number;
 
+  @UpdateDateColumn()
   @Field((type) => Int)
   updated_at: number;
 }
