28603f1d89ba8b5c4cf9bcb4d2139e0bad22c8ad
Harry <stuartharry0313@gmail.com>
2022-08-08 21:52:06 +0100

    Add dotenv, typeorm and mysql to project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,27 @@
+import { config } from "dotenv";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+
+config({ path: join(process.cwd(), 'src/.env') })
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: process.env.MYSQL_PASSWORD,
+      synchronize: true,
+      entities: ['dist/**/*.entity.{js,ts}']
     }),
     UserModule,
   ],
