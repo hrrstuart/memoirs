297e83127aee00cde2391620f433d410a9f1ae04
Harry <stuartharry0313@gmail.com>
2022-08-08 23:56:14 +0100

    Fix database not found

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,12 +17,13 @@ config({ path: join(process.cwd(), 'src/.env') })
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     TypeOrmModule.forRoot({
-      database: 'mysql',
+      database: 'atorus',
       type: 'mysql',
       username: 'root',
       port: 3306,
       password: process.env.MYSQL_PASSWORD,
-      entities: ['dist/**/*.entity.{js,ts}']
+      entities: ['dist/**/*.entity{.js,.ts}'],
+      synchronize: true
     }),
     UserModule,
   ],
