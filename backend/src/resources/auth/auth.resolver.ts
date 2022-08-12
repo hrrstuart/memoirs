73a8337079d7b542b13de5a012d6bb897256cf05
Harry <stuartharry0313@gmail.com>
2022-08-12 23:50:16 +0100

    Add test query to test for user session

--- backend/src/resources/auth/auth.resolver.ts
+++ backend/src/resources/auth/auth.resolver.ts
@@ -1,6 +1,7 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { AuthenticatedGuard } from './authenticated.guard';
 import { LoginResponse } from './dto/login-response';
 import { LoginUserInput } from './dto/login-user.input';
 import { GqlAuthGuard } from './gql-auth.gaurd';
@@ -12,8 +13,12 @@ export class AuthResolver {
     @Mutation(() => LoginResponse)
     @UseGuards(GqlAuthGuard)
     login(@Args('loginUserInput') loginUserInput: LoginUserInput, @Context() context) {
-        console.log('Session ID: ', context.req.sessionID);
-        console.log('Other session: ', context.req.session);
+        return this.authService.login(context.req.user);
+    }
+
+    @Query(() => LoginResponse)
+    @UseGuards(AuthenticatedGuard)
+    getSomething(@Context() context) {
         return this.authService.login(context.req.user);
     }
 }
