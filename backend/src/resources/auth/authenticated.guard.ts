581072dd5a8252feee2baa4ee64032dbbba6e19e
Harry <stuartharry0313@gmail.com>
2022-08-12 23:49:58 +0100

    Add authenticated guard to verify user session

---
+++ backend/src/resources/auth/authenticated.guard.ts
@@ -0,0 +1,11 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+@Injectable()
+export class AuthenticatedGuard implements CanActivate {
+    async canActivate(context: ExecutionContext) {
+        const ctx = GqlExecutionContext.create(context)
+        const req = ctx.getContext().req
+        return req.isAuthenticated();
+    }
+}
