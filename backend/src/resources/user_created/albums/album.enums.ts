eae73b5081fb5a2a1da41c728218fecb280ae3b2
hrrstuart <stuartharry0313@gmail.com>
2022-08-25 14:11:08 +0000

    Add enum type for privacy

---
+++ backend/src/resources/user_created/albums/album.enums.ts
@@ -0,0 +1,8 @@
+//  Who should be able to view/upload/retrieve photos from the album
+export enum Privacy {
+    EVERYONE,
+    FRIENDS_OF_FRIENDS,
+    FRIENDS_ONLY,
+    MEMBERS_ONLY,
+    OWNER_ONLY
+}
