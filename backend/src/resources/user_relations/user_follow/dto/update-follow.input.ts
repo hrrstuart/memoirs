6a4c998108cb5fb5ff0783cef636534d3406d010
Harry <stuartharry0313@gmail.com>
2022-08-28 15:56:12 +0100

    Delete update follows

--- backend/src/resources/user_relations/user_follow/dto/update-follow.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateFollowInput } from './create-user_follow.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateFollowInput extends PartialType(CreateFollowInput) {
-  @Field(() => Int)
-  id: number;
-}
