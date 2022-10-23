2d1f7c4c3432071fadfda6e45231ebe7d8c4200b
Harry <stuartharry0313@gmail.com>
2022-10-23 16:50:05 +0100

    Add method to create user and find all users

--- backend/src/users/controllers/users/users.controller.ts
+++ backend/src/users/controllers/users/users.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, HttpException, HttpStatus, Param, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Req, Res } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { UsersService } from 'src/users/services/users/users/users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
-    @Get(':id')
-    getUser(
-        @Param('username') username: string,
-    ) {
+    @Get('')
+    getUsers() {
+        return this.usersService.getAllUsers();
+    }
+
+    @Get('/search/:username')
+    getUser(@Param('username') username: string) {
         const user = this.usersService.findUserByUsername(username);
 
         if (user) return user;
         else throw new HttpException('User not found', HttpStatus.BAD_REQUEST)
+    }
+
+    @Post('create')
+    createUser(@Body() createUserDto: CreateUserDto) {
+        this.usersService.createUser(createUserDto);
     }
 
 }
--- backend/src/users/services/users/users/users.service.ts
+++ backend/src/users/services/users/users/users.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { User } from 'src/users/types/User';
 
 @Injectable()
 export class UsersService {
-    users = [{
+    private users: User[] = [{
         id: 'a',
+        email: 'rr@gmail.com',
         username: 'rr'
     }, {
         id: 'b',
+        email: 'hrrstuart@gmail.com',
         username: 'hrrstuart'
     }]
+
+    getAllUsers() {
+        return this.users;
+    }
 
     findUserByUsername(username: string) {
         return this.users.find(u => u.username === username);
     }
 
+    createUser(userDto: CreateUserDto) {
+        this.users.push(userDto);
+    }
 }
