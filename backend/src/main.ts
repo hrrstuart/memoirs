fa3ad22f36bcef1df87e07b88776104ef1321797
Harry <stuartharry0313@gmail.com>
2022-11-03 14:03:35 +0000

    Add express-session to project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
+import { join } from 'path';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
+
+config({ path: join(process.cwd(), 'src/.env') })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: (1000 * 60 * 60 * 24 * 365)
+    }
+  }))
   await app.listen(3000);
 }
 bootstrap();
