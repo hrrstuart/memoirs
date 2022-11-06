fcee2cc739fe56a9329e68f858316d017353e2b0
Harry <stuartharry0313@gmail.com>
2022-11-06 12:32:45 +0000

    Add Post to entities

--- backend/src/typeorm/index.ts
+++ backend/src/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { Album } from "./entities/Album";
+import { Post } from "./entities/Post";
 import { SessionEntity } from "./entities/Session";
 import { User } from "./entities/User";
 
-export { Album, User, SessionEntity };
+export { Album, User, SessionEntity, Post };
 
-const entities = [Album, User, SessionEntity];
+const entities = [Album, Post, User, SessionEntity];
 export default entities;
