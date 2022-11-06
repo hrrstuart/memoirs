cc619a26e86b415f594c087bc4b85600ffe1aa81
Harry <stuartharry0313@gmail.com>
2022-11-06 12:33:12 +0000

    Refactor add return value type

--- backend/src/albums/controllers/albums/albums.controller.ts
+++ backend/src/albums/controllers/albums/albums.controller.ts
@@ -2,7 +2,7 @@ import { Body, ClassSerializerInterceptor, Controller, Get, Post, UseGuards, Use
 import { CreateAlbumDto } from 'src/albums/dtos/CreateAlbum.dto';
 import { AlbumsService } from 'src/albums/services/albums/albums.service';
 import { AuthenticatedGuard } from 'src/auth/utils/LocalGuard';
-import { User } from 'src/typeorm';
+import { Album, User } from 'src/typeorm';
 import { AuthUser } from 'src/utils/decorators';
 
 @Controller('albums')
@@ -20,8 +20,8 @@ export class AlbumsController {
     @UseInterceptors(ClassSerializerInterceptor)
     @Post('create')
     @UsePipes(ValidationPipe)
-    async createAlbum(@AuthUser() user: User, @Body() createAlbumDto: CreateAlbumDto) {
-        const newAlbum = await this.albumsService.createAlbum(createAlbumDto, user)
+    async createAlbum(@AuthUser() user: User, @Body() createAlbumDto: CreateAlbumDto): Promise<Album> {
+        const newAlbum = await this.albumsService.createAlbum(createAlbumDto, user);
         return newAlbum;
     }
 }
