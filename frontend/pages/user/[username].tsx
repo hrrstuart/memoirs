ad08be77a17aaef769a778dc94b086c67189ff16
hrrstuart <stuartharry0313@gmail.com>
2023-06-14 23:58:39 +0100

    Highlight active tab

--- frontend/pages/user/[username].tsx
+++ frontend/pages/user/[username].tsx
@@ -8,11 +8,11 @@ function User() {
   
   // Sample photo list
   const photoList: IPost[] = [
-    { caption: "Life must be the craziest things and when you get to enjoy it, it can be pretty mad. Here is a picture of my satellite in the 3rd person POV. Enjoy!", image: "https://64.media.tumblr.com/d28ed0d493a7794b9a7b490e3d6612ea/tumblr_pdrm0xx05J1wvnafdo1_1280.jpg", likes: 841, owner: "hrrstuart" },
-    { caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/spy-satellite-orbiting-earth-nasa-public-domain-imagery-picture-id1201649930?s=2048x2048", likes: 1238, owner: "hrrstuart" },
-    { caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/uschi-glas-dieter-hermann-and-heidi-kranz-attend-the-bild-place-to-b-picture-id462960080?s=2048x2048", likes: 2922, owner: "hrrstuart" },
-    { caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/van-halen-having-a-crazy-party-eating-hamburgers-and-potatoes-tokyo-picture-id593327617?s=2048x2048", likes: 2292, owner: "hrrstuart" },
-    { caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/group-of-young-people-having-water-fight-in-field-laughing-picture-id200250724-002", likes: 13812, owner: "hrrstuart" }
+    { createdAt: 1665822600, caption: "Life must be the craziest things and when you get to enjoy it, it can be pretty mad. Here is a picture of my satellite in the 3rd person POV. Enjoy!", image: "https://64.media.tumblr.com/d28ed0d493a7794b9a7b490e3d6612ea/tumblr_pdrm0xx05J1wvnafdo1_1280.jpg", likes: 841, owner: "hrrstuart" },
+    { createdAt: 1675699500, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/spy-satellite-orbiting-earth-nasa-public-domain-imagery-picture-id1201649930?s=2048x2048", likes: 1238, owner: "hrrstuart" },
+    { createdAt: 1667771700, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/uschi-glas-dieter-hermann-and-heidi-kranz-attend-the-bild-place-to-b-picture-id462960080?s=2048x2048", likes: 2922, owner: "hrrstuart" },
+    { createdAt: 1673778600, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/van-halen-having-a-crazy-party-eating-hamburgers-and-potatoes-tokyo-picture-id593327617?s=2048x2048", likes: 2292, owner: "hrrstuart" },
+    { createdAt: 1670016000, caption: "Live, love, laugh", image: "https://media.gettyimages.com/photos/group-of-young-people-having-water-fight-in-field-laughing-picture-id200250724-002", likes: 13812, owner: "hrrstuart" }
   ];
 
   // Sample album list
@@ -25,10 +25,35 @@ function User() {
     { albumID: 'dkalDka2A', thumbnail: "https://media.gettyimages.com/photos/colorful-aurora-borealis-picture-id1049922746?s=2048x2048", title: "Iceland trip", photos: 191 },
   ];
   const router = useRouter();
-  const [toDisplay, setToDisplay] = useState<IPost[] | any[]>(photoList);
+  const [toDisplay, setToDisplay] = useState<IPost[] | any[]>(photoList.sort((a, b) => b.createdAt - a.createdAt));
+  const [activeTab, setActiveTab] = useState<number>(0);
 
   // Get toDisplay state from local cache. It says 'userID/state' and if there's no matching userID, it defaults to photoList
 
+  
+  const tabs = [
+    { 
+      label: "Photos",
+      id: 0,
+      onClick: () => {
+        setToDisplay(photoList);
+        setActiveTab(0);
+      }
+    },
+    { 
+      label: "Albums",
+      id: 1,
+      onClick: () => {
+        setToDisplay(albumList);
+        setActiveTab(1);
+      }
+    },
+    { 
+      label: "Activity",
+      id: 2,
+      onClick: () => {}
+    },
+  ]
   const userInfo = {
     username: router.query.username as string,
     realName: "Harry Stuart",
@@ -48,11 +73,17 @@ function User() {
           text={{ title: userInfo.username, realName: userInfo.realName, description: userInfo.description }}
           thumbnail={userInfo.pfp}
       />
-      <div className="flex flex-row mt-5 border-t">
+      <div className="flex flex-row mt-10">
         <nav className='flex flex-row mx-auto justify-between w-full pt-2'>
-          <button onClick={() => setToDisplay(photoList)} className={`uppercase focus:bg-slate-700 hover:bg-slate-700 rounded-xl px-5 py-2 duration-100`}>Photos</button>
-          <button onClick={() => setToDisplay(albumList)} className={`uppercase focus:bg-slate-700 hover:bg-slate-700 rounded-xl px-5 py-2 duration-100`}>Albums</button>
-          <button className='uppercase hover:bg-slate-700 rounded-xl px-5 py-2 duration-100'>Activity</button>
+            {tabs.map((tab, i) => (
+              <div
+                key={tab.id}
+                className={`tab ${activeTab === tab.id ? 'border-b' : 'border-none'} cursor-pointer uppercase hover:bg-slate-700/30 px-5 py-2 duration-100`}
+                onClick={() => tab.onClick()}
+              >
+                {tab.label}
+              </div>
+            ))}
         </nav>
       </div>
       <div>
