b6627a9658012293c29b2500d3d18b16f9156f90
hrrstuart <stuartharry0313@gmail.com>
2023-06-16 23:59:42 +0100

    Remove sidebar from specified routes

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,8 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const noSidebarRoutes = ['/login'];
+  const shouldRenderSidebar = !noSidebarRoutes.includes(router.pathname);
+
   return (
     <>
       <Head>
@@ -12,9 +18,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <div className='bg-[#111111] flex flex-col min-h-screen'>
         <main className='flex flex-grow flex-row'>
-          <div className='h-screen sticky top-0 hidden md:block lg:ml-32'>
-            <Sidebar />
-          </div>
+          { shouldRenderSidebar &&
+            <div className='h-screen sticky top-0 hidden md:block lg:ml-32'>
+              <Sidebar />          
+            </div>
+          }
           <div className='mx-auto p-5'>
             <Component {...pageProps} />
           </div>
